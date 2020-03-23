baa4e3c0faf0893710a66ef7b19760d8f402f8ba
Enor Anidi-Ryz <hello@enoranidi.com>
2020-03-23 15:54:56 +0000

    application status work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,15 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  	window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
 $(document).ready(function () {
-  window.GOVUKFrontend.initAll()
+  	window.GOVUKFrontend.initAll()
 })
+
+// MOJ Multi select component
+new MOJFrontend.MultiSelect({
+	container: $('.moj-multi-select__select-all-container'),
+	checkboxes: $('.govuk-checkboxes__input')
+});
