f856aa552e5e7f90b4099b0024a54127dc2a173d
Enor Anidi-Ryz <hello@enoranidi.com>
2020-04-15 15:04:52 +0100

    Updates to prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,22 @@ new MOJFrontend.MultiSelect({
 new MOJFrontend.SortableTable({
 	table: $('table')[0]
 });
+
+// MOT 'Filter a list' pattern
+new MOJFrontend.FilterToggleButton({
+    bigModeMediaQuery: '(min-width: 48.063em)',
+    startHidden: true,
+    toggleButton: {
+		container: $('.moj-action-bar__filter'),
+		showText: 'Show filter',
+		hideText: 'Hide filter',
+		classes: 'govuk-button--secondary'
+    },
+    closeButton: {
+		container: $('.moj-filter__header-action'),
+		text: 'Close'
+    },
+    filter: {
+      	container: $('.moj-filter')
+    }
+  });
