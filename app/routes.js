26d972d5eae1a7c0f7f8d14df2f1fe4c398a66fe
Enor Anidi-Ryz <hello@enoranidi.com>
2020-04-27 12:54:04 +0100

    Updated routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+// compare arrays function
+const arraysAreEqual = function(a, b){
+	return JSON.stringify(a) == JSON.stringify(b)
+  }
+
 // Add your routes here - above the module.exports line
 
 router.post('/view-process-applications/v1/stages/application-jac00105-router', function (req, res) {
 	
 	let applicationJac00105 = req.session.data['application-jac00105'] || []
 	
+	// Only one option selected - compare arrays function 
+	// e.g. if (arraysAreEqual(applicationJac00105, ['jac00105-abc0001'])) {
+		
 	if (applicationJac00105.includes('jac00105-abc0001')) {
 		res.redirect('/view-process-applications/v1/stages/applied-jac00105-abc0001')
 	} else if (applicationJac00105.includes('jac00105-abc0002') && applicationJac00105.includes('jac00105-abc0003') && applicationJac00105.includes('jac00105-abc0004') && applicationJac00105.includes('jac00105-abc0005')) {
 		res.redirect('/view-process-applications/v1/stages/bulk-status')
-    } else if (applicationJac00105.includes('jac00105-abc0006')) {
+	} else if (applicationJac00105.includes('jac00105-abc0006')) {
 		res.redirect('/view-process-applications/v1/stages/applied-jac00105-abc0006')
-    } else {
+	} else {
 		res.redirect('/view-process-applications/v1/stages/applied')
 	}
 })
