89fadf564f2e51c8b12e5e806ea0aef50370f99a
Enor Anidi-Ryz <hello@enoranidi.com>
2020-05-06 12:50:19 +0100

    Updates to prototype

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Start folder specific routes
-
+router.use('/v1', require('./views/v1/_routes'));
 // current sprint, remember to add older sprint when adding a new folder!
 router.use('/current', require('./views/current/_routes'));
 
